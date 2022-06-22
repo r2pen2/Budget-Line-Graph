ef36af2ca8b17f3e1755f4c8048aa9df8a717cbc
r2pen2 <joedobbelaar@gmail.com>
2022-06-22 14:31:48 -0400

    Initial Commit v.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss"
+import { ThemeProvider, Typography, Stack } from "@mui/material"
+
+import { BrowserRouter as Router } from "react-router-dom";
+import Topbar from "./components/topbar/Topbar"
+import BottomNav from "./components/bottomnav/BottomNav"
+import SpinningLogo from "./components/spinningLogo/SpinningLogo"
+
+import theme from "./assets/style/theme"
 
 function App() {
+
+  const showBottomNav = false;
+
+  function renderBottomNav() {
+    if (showBottomNav) {
+      return <BottomNav />
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Router>
+    <ThemeProvider theme={theme}>
+      <Topbar />
+        <Stack className="content">
+          <SpinningLogo />
+          <Typography component="div" variant="h4" marginTop="50px">Citrus | Coming Soon!</Typography>
+        </Stack>
+      { renderBottomNav() }
+    </ThemeProvider>
+  </Router>
+  )
 }
+
+
 
 export default App;
