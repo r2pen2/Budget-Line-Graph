e57926007d8866c97dce30c8eb3882c2bb999ed2
r2pen2 <joedobbelaar@gmail.com>
2022-06-22 14:43:32 -0400

    Initial Commit v.3

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.scss"
+import { ThemeProvider } from "@mui/material"
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+import theme from "./assets/style/theme"
+
+function App() {
+
+  return (
+    <div className="app">
+      <Router>
+      </Router>
+    </div>
+  )
+}
+
+
+
+export default App;
--- src/components/spinningLogo/SpinningLogo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import logo from '../../assets/images/Logo256.png'
-import "./spinninglogo.scss"
-
-export default function SpinningLogo() {
-  return (
-    <img src={logo} alt="logo" className="logo"/>
-  )
-}
